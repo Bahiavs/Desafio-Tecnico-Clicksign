22fce13f948d8475f6a8090581f38d63b2849b13
Vinicius Bahia <vini.bahiarj@gmail.com>
2024-11-02 03:04:04 -0300

    It adds assortments

--- src/pages/project-listing/project-listing.component.ts
+++ src/pages/project-listing/project-listing.component.ts
@@ -14,15 +14,31 @@ export class ProjectListingComponent {
     readonly projects$ = inject(GetProjectsService).projects$
     readonly filterTextControl = new FormControl('')
     readonly filterStarredControl = new FormControl(false)
+    readonly assortmentControl = new FormControl<'alphabetical' | 'mostRecentStarted' | 'closerToCompletionDate'>('alphabetical')
     readonly filteredProjects$ = combineLatest([
         this.projects$,
         this.filterTextControl.valueChanges.pipe(startWith(this.filterTextControl.value)),
         this.filterStarredControl.valueChanges.pipe(startWith(this.filterStarredControl.value)),
+        this.assortmentControl.valueChanges.pipe(startWith(this.assortmentControl.value)),
     ]).pipe(
-        map(([projects, filterText, isFilteringStarred]) => projects.filter(project => {
-            if (isFilteringStarred && !project.isStarred) return false
-            if (!project.name.includes(filterText!)) return false
-            return true
-        }))
+        map(([projects, filterText, isFilteringStarred, assortment]) => {
+            let filteredProjects = projects.filter(project => {
+                if (isFilteringStarred && !project.isStarred) return false
+                if (!project.name.includes(filterText!)) return false
+                return true
+            })
+            switch (assortment) {
+                case 'alphabetical':
+                    filteredProjects = filteredProjects.sort((a, b) => a.name.localeCompare(b.name))
+                    break
+                case 'mostRecentStarted':
+                    filteredProjects = filteredProjects.sort((a, b) => b.startDate.getTime() - a.startDate.getTime())
+                    break
+                case 'closerToCompletionDate':
+                    filteredProjects = filteredProjects.sort((a, b) => b.endDate.getTime() - a.endDate.getTime())
+                    break
+            }
+            return filteredProjects
+        })
     )
 }
